8b66b8078f0aca28c25aaa365b5dfb1537fe3a18
Lois Desplat <lois.desplat@gmail.com>
2015-03-16 11:46:10 -0700

    Update to React 0.13. Use Babel instead of react-tools. Update our testing tools.

--- lab-transform.js
+++ lab-transform.js
@@ -1,8 +1,20 @@
-var ReactTools = require('react-tools');
+var Babel = require('babel-core');
 
 module.exports = [{
-	ext: '.js',
-	transform: function (content, filename) {
-		return ReactTools.transform(content);
-	}}
-];
+    ext: '.jsx',
+    transform: function (content, filename) {
+
+        if (filename.indexOf('node_modules') === -1) {
+            var options = {
+                sourceMap: 'inline',
+                filename: filename,
+                sourceFileName: filename
+            };
+            var result = Babel.transform(content, options);
+            
+            return result.code;
+        }
+
+        return content;
+    }
+}];
--- webpack.config.js
+++ webpack.config.js
@@ -37,7 +37,7 @@ module.exports = {
 
   module: {
     loaders: [
-      { test: /\.js$/, loader: 'jsx-loader?harmony' }
+      { test: /\.jsx$/, loader: 'babel-loader' }
     ]
   },
 
